21b1dbace0bf8980c2de7089ab6bdcd036263328
Anggoro Gagah <aak270@gmail.com>
2024-07-09 19:04:28 +0700

    add onboard image

---
+++ lib/views/index/login_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(children: [
+          Text("Login to your account"),
+        ]),
+      ),
+    );
+  }
+}
