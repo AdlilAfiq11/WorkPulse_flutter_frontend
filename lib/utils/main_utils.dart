6584a51bec9ca7406253731fc12934bf1a4b4f9d
AfiqNSE <afiq@nse.com.my>
2024-07-14 20:01:04 +0800

    Update main_utils

--- lib/utils/main_utils.dart
+++ lib/utils/main_utils.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:workpulse_flutter_frontend/models/color_model.dart';
+import 'package:workpulse_flutter_frontend/views/jobs/job_pos.dart';
 
 Color jobStatusColor(String jobStatus) {
   switch (jobStatus) {
@@ -18,25 +19,31 @@ Widget bottomButton(
   required String title,
   required Function()? onPressed,
 }) =>
-    ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        side: BorderSide(
-          color: AppColor.blueViolet,
+    Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          top: BorderSide(color: Color.fromRGBO(241, 241, 241, 1)),
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
-        ),
-        minimumSize: const Size(double.infinity, 50),
-        backgroundColor: AppColor.blueViolet,
-        disabledBackgroundColor: const Color.fromRGBO(31, 48, 94, .5),
-        elevation: 5,
       ),
-      onPressed: onPressed,
-      child: Text(
-        title,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 17,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: AppColor.blueViolet,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          minimumSize: const Size(double.infinity, 50),
+          disabledBackgroundColor: const Color.fromRGBO(31, 48, 94, .5),
+          elevation: 5,
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+          ),
         ),
       ),
     );
@@ -71,6 +78,78 @@ ToastFuture customShowToast(
       fullWidth: true,
       textAlign: TextAlign.justify,
     );
+
+Future<dynamic> photoAlert(
+  BuildContext context,
+  String title, {
+  required Function()? onPressed,
+}) {
+  return showModalBottomSheet(
+    showDragHandle: true,
+    backgroundColor: Colors.white,
+    elevation: 5.0,
+    context: context,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+    ),
+    builder: (context) {
+      return SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(18, 10, 18, 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const SizedBox(height: 5),
+              Text(
+                title,
+                style: TextStyle(
+                  color: AppColor.matteBlack,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Uploading photo will help to avoid any unnecessary dispute from customer. It can save you from a lot of trouble later on.',
+                style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  side: BorderSide(
+                    color: AppColor.blueViolet,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                  backgroundColor: AppColor.blueViolet,
+                  disabledBackgroundColor: const Color.fromRGBO(31, 48, 94, .5),
+                  elevation: 5,
+                ),
+                onPressed: onPressed,
+                child: const Text(
+                  'Ok, I got it',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
 
 class SlideRoute extends PageRouteBuilder {
   final Widget page;
